d63ee9cfd339ab9c0c49a9e323fce5d960b600f9
AntipasBen23 <benantipas@gmail.com>
2026-01-20 12:31:32 +0100

    added kogi minerals lib

---
+++ src/lib/data/kogi-minerals.ts
@@ -0,0 +1,78 @@
+export const kogiMinerals = [
+  {
+    name: "Gold",
+    symbol: "Au",
+    width: "6m",
+    depth: "5m",
+  },
+  {
+    name: "Copper",
+    symbol: "Cu",
+    width: "5m",
+    depth: "5m",
+  },
+  {
+    name: "Hydrogen Gas",
+    symbol: "H2",
+  },
+  {
+    name: "Ruby",
+    symbol: "Al2O3",
+  },
+  {
+    name: "Tungsten Trioxide",
+    symbol: "WO3",
+  },
+  {
+    name: "Barium",
+    symbol: "Ba",
+  },
+  {
+    name: "Glass Stone",
+    symbol: "SiO2",
+  },
+  {
+    name: "Coal",
+    symbol: "C",
+  },
+  {
+    name: "Fluorite",
+    symbol: "CaF2",
+  },
+  {
+    name: "Nickel Oxide",
+    symbol: "NiO",
+  },
+  {
+    name: "Polonium Dioxide",
+    symbol: "PoO2",
+  },
+  {
+    name: "Platinum Chloride",
+    symbol: "PtCl2",
+  },
+  {
+    name: "Agate",
+    symbol: "SiO2",
+  },
+  {
+    name: "Aluminium",
+    symbol: "Al",
+  },
+  {
+    name: "Gypsum",
+    symbol: "CaSO4",
+  },
+  {
+    name: "Zircon",
+    symbol: "ZrSiO4",
+  },
+  {
+    name: "Monazite",
+    symbol: "CePO4",
+  },
+  {
+    name: "Germanium Dioxide",
+    symbol: "GeO2",
+  },
+];
